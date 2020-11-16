dd312a6e7af22ef8f88dbc3b2ea058a44e733577
forexms78 <49295076+forexms78@users.noreply.github.com>
2020-11-16 22:48:30 +0900

    3-1

---
+++ AreaTest.java
@@ -0,0 +1,16 @@
+
+public class AreaTest {
+	public static void main(String[] args) {
+		double radius, area;
+		radius = 5.0;
+		area = 3.1411592 * radius * radius;
+		System.out.println("원의 면적은 " + area);
+		
+		//float temperature =  12.3;
+		float temperature = (float) 12.3;
+		
+		float temperature1 = 12.3F;
+		
+		double number = 123_456_789.0;
+	}
+}
